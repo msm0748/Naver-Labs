03f0622d3036c6cc55f241bb36748e7a3bbd5eff
Moon-Seokmin <msm0748@gmail.com>
2021-10-25 00:03:59 +0900

    first commit

--- ie.js
+++ ie.js
@@ -1,32 +1,9 @@
-// const nav = document.getElementById('nav');
-// const rectTop = nav.getBoundingClientRect().top;
-
-// window.addEventListener('scroll', function() {
-//   if (window.pageYOffset > rectTop) {
-//     nav.style.position = 'fixed';
-//     nav.style.top = 0;
-//   } else {
-//     nav.style.position = 'static';
-//     nav.style.top = '';
-//   }
-// });
-
-
-
-
-const nav = document.getElementById('nav');
-const tmp = nav.cloneNode(true);
-tmp.style.visibility = 'hidden';
-const rectTop = nav.getBoundingClientRect().top;
-
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > rectTop) {
-    nav.parentNode.appendChild(tmp);
-    nav.style.position = 'fixed';
-    nav.style.top = 0;
-  } else {
-    nav.parentNode.removeChild(tmp);
-    nav.style.position = 'static';
-    nav.style.top = '';
-  }
+function ask(cd){
+  setTimeout(function(){
+  console.log("---start---");
+    cd();
+  }, 200);
+}
+ask(function(){
+console.log("---end---")
 });
